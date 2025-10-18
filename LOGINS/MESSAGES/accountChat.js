a13ddaf7ff4f8865965113284ba5c492bf608cbb
FACETOFACEASSEMBLY <praiseoverah@gmail.com>
2025-10-18 14:22:24 +0100

    Add files via upload

---
+++ LOGINS/MESSAGES/accountChat.js
@@ -0,0 +1,65 @@
+import { chatMessageAghatise, chatMessageIsaac } from "./chatSec.js";
+import { userEmail } from "../Accounts/PROFILES/profiles.js";
+
+function aghatiseMessagePage() {
+    let chatMessageHTML = '';
+
+chatMessageAghatise.forEach((chatInfo) => {
+    const username = chatInfo.name;
+    const userImage = chatInfo.img;
+    const recentMessage = chatInfo.recentMessage;
+    const unreadMessages = chatInfo.unreadMessages;
+    
+    chatMessageHTML += `
+            <div class="message-item">
+        <div class="profile-wrapper">
+        <img src="${userImage}" alt="Profile">
+        <span class="online-dot"></span>
+        </div>
+        <div class="message-info">
+        <h2>${username}</h2>
+        <p>${recentMessage}</p>
+        </div>
+        <div class="message-meta">
+        <span class="time">10:30 AM</span>
+        ${unreadMessages}
+        </div>
+    </div>
+    `;
+    document.getElementById('messages-js').innerHTML = chatMessageHTML;
+});
+}
+function isaacMessagePage() {
+    let chatMessageHTML = '';
+
+chatMessageIsaac.forEach((chatInfo) => {
+    const username = chatInfo.name;
+    const userImage = chatInfo.img;
+    const recentMessage = chatInfo.recentMessage;
+    const unreadMessages = chatInfo.unreadMessages;
+    
+    chatMessageHTML += `
+            <div class="message-item">
+        <div class="profile-wrapper">
+        <img src="${userImage}" alt="Profile">
+        <span class="online-dot"></span>
+        </div>
+        <div class="message-info">
+        <h2>${username}</h2>
+        <p>${recentMessage}</p>
+        </div>
+        <div class="message-meta">
+        <span class="time">10:30 AM</span>
+        ${unreadMessages}
+        </div>
+    </div>
+    `;
+    document.getElementById('messages-js').innerHTML = chatMessageHTML;
+});
+}
+
+if (userEmail === 'praiseoverah@gmail.com') {
+    isaacMessagePage();
+} else if (userEmail === 'aghatise@gmail.com') {
+    aghatiseMessagePage();
+}
---
+++ LOGINS/MESSAGES/chatSec.js
@@ -0,0 +1,81 @@
+export const chatMessageIsaac= [{
+    name: 'Aghatise',
+    img: '../Accounts/aghatise ehioghae pfp.jpg',
+    recentMessage: `Bro have you applied yet!?`,
+    unreadMessages: `<span style = "font-size: 12px;
+  color: #999; /* gray ticks (not blue) */
+  margin-left: 4px;" class="message-ticks">✔✔</span>
+`,
+}, {
+    name: 'Fort',
+    img: '../Accounts/PROFILE-IMAGES/67c8ca63-85d7-46d3-8935-31d691f3dd7a.jpg',
+    recentMessage: `How's the business?`,
+    unreadMessages: ` <span style="background: #762dec;   
+  color: white;
+  font-size: 12px;
+  font-weight: bold;
+  min-width: 18px;
+  height: 18px;
+  line-height: 18px;
+  text-align: center;
+  border-radius: 50%;
+  display: inline-block;" class="unread-count">5</span>`,
+}, {
+    name: 'Light',
+    img: '/LOGINS/Accounts/PROFILE-IMAGES/62f4d8a1-b26c-49a5-bddf-8bca2264bf26.jpg',
+    recentMessage: `My brother wassup`,
+    unreadMessages: ` <span style="background: #762dec;   
+  color: white;
+  font-size: 12px;
+  font-weight: bold;
+  min-width: 18px;
+  height: 18px;
+  line-height: 18px;
+  text-align: center;
+  border-radius: 50%;
+  display: inline-block;" class="unread-count">1</span>`,
+}];
+
+export const chatMessageAghatise= [{
+    name: 'Isaac-Praise',
+    img: '/LOGINS/Accounts/isaac profile.jpg',
+    recentMessage: `Bro have you applied yet!?`,
+    unreadMessages: ` <span style="background: #762dec;   
+  color: white;
+  font-size: 12px;
+  font-weight: bold;
+  min-width: 18px;
+  height: 18px;
+  line-height: 18px;
+  text-align: center;
+  border-radius: 50%;
+  display: inline-block;" class="unread-count">1</span>`
+}, {
+    name: 'Fort',
+    img: '../Accounts/PROFILE-IMAGES/67c8ca63-85d7-46d3-8935-31d691f3dd7a.jpg',
+    recentMessage: `Hope on the game bro`,
+    unreadMessages: ` <span style="background: #762dec;   
+  color: white;
+  font-size: 12px;
+  font-weight: bold;
+  min-width: 18px;
+  height: 18px;
+  line-height: 18px;
+  text-align: center;
+  border-radius: 50%;
+  display: inline-block;" class="unread-count">5</span>`
+}, {
+    name: 'Light',
+    img: '/LOGINS/Accounts/PROFILE-IMAGES/62f4d8a1-b26c-49a5-bddf-8bca2264bf26.jpg',
+    recentMessage: `How's it like having fur??`,
+    unreadMessages: ` <span style="background: #762dec;   
+  color: white;
+  font-size: 12px;
+  font-weight: bold;
+  min-width: 18px;
+  height: 18px;
+  line-height: 18px;
+  text-align: center;
+  border-radius: 50%;
+  display: inline-block;" class="unread-count">2</span>`
+}]
